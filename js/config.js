f358b96fb01e57ed157869f6d9b315bf3179912e
David Millan <damillano93@gmail.com>
2023-01-04 11:29:25 -0500

    add cognito config

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 window._config = {
     cognito: {
-        userPoolId: 'Id de grupo us-east-1_8q8R61WpG', // e.g. us-east-2_uXboG5pAb
+        userPoolId: 'us-east-1_8q8R61WpG', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '1dncnhea6fcli2r09ri7v1f5it', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
