711dd91bb9ca48925a1f8d6eb6633244d36e5828
David Millan <damillano93@gmail.com>
2023-01-04 11:24:49 -0500

    add cognito config

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'Id de grupo us-east-1_8q8R61WpG', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '1dncnhea6fcli2r09ri7v1f5it', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
